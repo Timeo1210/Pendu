ad4dafda78a258f0debeaf92c5e75f9e2f83d88c
timeo1210 <timeo.boulhol@gmail.com>
2019-03-03 16:14:15 +0100

    Helol

--- client/html_Game/main.js
+++ client/html_Game/main.js
@@ -1,5 +1,5 @@
 var socket = io();
-var allSpan = ['Init', 'Init_after_name','wtgPlayer']
+var allSpan = ['Init', 'Init_after_name','wtgPlayer', 'wordChoose', 'Pendu']
 function InitingAff() {
     for (let j = 1; j < allSpan.length; j++) {
         document.getElementById(allSpan[j]).style.display = 'none'
@@ -9,8 +9,12 @@ InitingAff()
 
 var allParty_id = []
 var myId = ''
-var indexparty = ''
+var indexParty = ''
 var admin = false;
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var letterTurn = false
+var mistakeAffInit = false
+var partyEnd = false
 
 socket.emit('get_allParty_id')
 
@@ -23,7 +27,7 @@ socket.on('allParty_id', function(data) {
 })
 
 socket.on('infoMyParty', function(data) {
-    console.log('test')
+    indexParty = data.index;
     //dysplay: none Init
     document.getElementById('Init').style.display = 'none';
     document.getElementById('wtgPlayer').style.display = 'block';
@@ -74,6 +78,68 @@ socket.on('infoMyParty', function(data) {
 
 })
 
+socket.on('wordTurn', function(data) {
+    document.getElementById('wtgPlayer').style.display = 'none';
+    document.getElementById('Pendu').style.display = 'none';
+    document.getElementById('wordChoose').style.display = 'block';
+
+    partyEnd = false
+    letterTurn = false
+    document.getElementById('p_endOutput').innerHTML = ''
+    document.getElementById('bnt_nextParty').disabled = true
+    AffLetters(['NONE'], ['NONE'])
+    AffMistake(0)
+
+    if (data === socket.id) {
+        document.getElementById('wordChoose_true').style.display = 'block'
+    } else {
+        document.getElementById('wordChoose_true').style.display = 'none'
+    }
+})
+
+socket.on('wordProcess', function(data) {
+    document.getElementById('wordChoose').style.display = 'none'
+    document.getElementById('input_word_Pendu').value = ''
+
+    document.getElementById('Pendu').style.display = 'block'
+    //dsa   zdadadad
+
+
+    AffWord(data.word)
+    AffLetters(data.word, data.letter)
+    if (mistakeAffInit === false) {
+        mistakeAffInit = true
+        AffMistake(0)
+    }
+})
+
+socket.on('mistakeLetter', function(data) {
+    AffMistake(data)
+})
+
+
+socket.on('letterTurn', function(data) {
+    if (data === socket.id) {
+        letterTurn = true
+    }
+})
+
+socket.on('partyEnd_Lose', function(data) {
+    partyEnd = true;
+    AffWord(data)
+    document.getElementById('p_endOutput').innerHTML = 'Vous avez Perdu !'
+    if (admin === true) {
+        document.getElementById('bnt_nextParty').disabled = false
+    }
+})
+
+socket.on('partyEnd_Win', function() {
+    partyEnd = true
+    document.getElementById('p_endOutput').innerHTML = 'Vous avez Gagnez !'
+    if (admin === true) {
+        document.getElementById('bnt_nextParty').disabled = false
+    }
+})
 
 function Aff_allParty_id() {
     allParty_id.unshift(['Nom', 'Nombre de Personne', 'ID de la partie'])
@@ -82,17 +148,32 @@ function Aff_allParty_id() {
     document.getElementById('list_allParty').appendChild(tbl);
     tbl.id = 'table_list_allParty';
     tbl.className = 'list_allParty';
+
+    let bool_do = true
     for (let j = 0; j < allParty_id.length; j++) {
         var tr = document.createElement('tr')
         tbl.appendChild(tr)
         for (let i = 0; i < allParty_id[j].length; i++) {
-            var td = document.createElement('td')
-            tr.appendChild(td)
-            td.innerHTML = allParty_id[j][i];
-            td.className = 'cell_list_allParty'
-            if (j !== 0) {
-                td.setAttribute("onclick", function(){var row = j - 1; JoinThis(allParty_id[row][2])})
-                td.onclick = function(){var row = j - 1; JoinThis(allParty_id[row][2])}
+            function aff_allPartyTD() {
+                var td = document.createElement('td')
+                tr.appendChild(td)
+                if (allParty_id[j][allParty_id.length - 1] !== true) {
+                    td.className = 'cell_list_allParty'
+                } else {
+                    td.className = 'cell_list_allParty cell_Red'
+                }
+
+                td.innerHTML = allParty_id[j][i];
+
+                if (j !== 0) {
+                    td.setAttribute("onclick", function(){var row = j - 1; JoinThis(allParty_id[row][2])})
+                    td.onclick = function(){var row = j - 1; JoinThis(allParty_id[row][2])}
+                }
+            }
+            if (j === 0) {
+                aff_allPartyTD()
+            } else if (i !== allParty_id[j].length - 1) {
+                aff_allPartyTD()
             }
         }
     }
@@ -115,7 +196,89 @@ function CreateParty() {
 }
 
 function StartGame() {
+    if (admin === true) {
+        socket.emit('getWordTurn', indexParty)
+    }
+}
 
+function NewWord() {
+    let word = document.getElementById('input_word_Pendu').value
+    socket.emit('wordIs', {
+        word: word,
+        index: indexParty,
+        letter: 'NONE'
+    })
+}
+
+function AffWord(wordP) {
+    document.getElementById('span_word_AFF').innerHTML = ''
+    var tbl = document.createElement('table')
+    document.getElementById('span_word_AFF').appendChild(tbl)
+    var tr = document.createElement('tr')
+    tbl.appendChild(tr)
+    for (let i = 0; i < wordP.length; i++) {
+        var td = document.createElement('td')
+        tr.appendChild(td)
+
+        td.innerHTML = wordP[i]
+    }
+
+}
+
+function AffLetters(letters, misLetter) {
+    document.getElementById('span_alphabet_AFF').innerHTML = ''
+    var tbl = document.createElement('table')
+    document.getElementById('span_alphabet_AFF').appendChild(tbl)
+    var tr = document.createElement('tr')
+    tbl.appendChild(tr)
+    for (let i = 0; i < alphabet.length; i++) {
+        var td = document.createElement('td')
+        tr.appendChild(td)
+        td.innerHTML = alphabet[i]
+        td.id = 'AffLetters' + '/' + i
+        if (letters.some(elem => elem.toLowerCase() === alphabet[i].toLowerCase()) !== true && misLetter.some(elem => elem.toLowerCase() === alphabet[i].toLowerCase()) !== true && partyEnd === false) {
+            td.setAttribute("onclick", function(){LetterChoice(this.innerHTML)})
+            td.onclick = function(){LetterChoice(this.innerHTML)}
+        } else {
+            td.className = 'cell_Red'
+        }
+    }
+}
+
+function LetterChoice(letter) {
+    if (letterTurn === true) {
+        letterTurn = false
+        socket.emit('newLetterChoice', {
+            index: indexParty,
+            letter: letter
+        })
+    }
+}
+
+function AffMistake(nbr_mistake) {
+    let tab_mistake = []
+    for (let i = 0; i < nbr_mistake; i++) {
+        tab_mistake.push('X')
+    }
+    document.getElementById('span_mistake_AFF').innerHTML = ''
+    var tbl = document.createElement('table')
+    tbl.style.border = '1px black solid'
+    tbl.style.height = '20px'
+    document.getElementById('span_mistake_AFF').appendChild(tbl)
+    var tr = document.createElement('tr')
+    tbl.appendChild(tr)
+    //nuber eroor = 8
+    for (let i = 0; i < 8; i++) {
+        var td = document.createElement('td')
+        tr.appendChild(td)
+        if (tab_mistake[i] === 'X') {
+            td.className = 'cell_Red'
+        }
+    }
+}
+
+function NextParty() {
+    socket.emit('getWordTurn', indexParty)
 }
 
 
@@ -130,7 +293,7 @@ socket.on('ERROR_001', function() {
     function endAnim() {
         document.getElementById('ERROR_001').innerHTML = ''
     }
-    document.getElementById('ERROR_001').innerHTML = "Partie Inexistante";
+    document.getElementById('ERROR_001').innerHTML = "Partie inexistante ou en cours";
     document.getElementById('ERROR_001').style.animation = 'fadeIn 1s'
     setTimeout(afterAnim, 900)
     setTimeout(endAnim, 1900)
--- server.js
+++ server.js
@@ -22,8 +22,8 @@ serv.listen(port, function() {
 });
 
 var allParty = [
-    ['id1', [['joueur1', 'IDofjoueur1'], ['joueur2', 'IDofjoueur2']], 'LapartyX', '', ''],
-    ['id2', [['joueur1', 'IDofjoueur1'], ['joueur2', 'IDofjoueur2']], 'LapartyX', '', '']
+    ['id1', [['joueur1', 'IDofjoueur1'], ['joueur2', 'IDofjoueur2']], 'LapartyX', -1, -1, '', false, [[], []], 0],
+    ['id2', [['joueur1', 'IDofjoueur1'], ['joueur2', 'IDofjoueur2']], 'LapartyX', -1, -1, '', false, [[], []], 0]
 ]
 
 var someName = ['Aple', 'Rat', 'Jimmy', 'Solomon', 'Rita']
@@ -33,14 +33,14 @@ io.sockets.on('connection', function(socket){
 
 
     function get_allParty_id(all) {
-        let data = [];
+        let dataT = [];
         for (let j = 0; j < allParty.length; j++) {
-            data.push([allParty[j][2], allParty[j][1].length.toString(), allParty[j][0]]);
+            dataT.push([allParty[j][2], allParty[j][1].length.toString(), allParty[j][0],  allParty[j][6]]);
         }
         if (all === false) {
-            socket.emit('allParty_id', data)
+            socket.emit('allParty_id', dataT)
         } else {
-            io.emit('allParty_id', data)
+            io.emit('allParty_id', dataT)
         }
     }
     socket.on('get_allParty_id', function() {
@@ -56,7 +56,7 @@ io.sockets.on('connection', function(socket){
 
             socket.join(id)
             for (let j = 0; j < allParty.length; j++) {
-                if (allParty[j][0] === id) {
+                if (allParty[j][0] === id && allParty[j][6] === false) {
                     let rdmId = socket.id;
                     allParty[j][1].push([name, rdmId])
                     let players = [[], []]
@@ -83,8 +83,94 @@ io.sockets.on('connection', function(socket){
     })
 
     socket.on('newpartie', function(data) {
-        allParty.push([socket.id, [], data.nameParty, '', ''])
+        allParty.push([socket.id, [], data.nameParty, -1, -1, '', false, [[], []], 0])
         UserJoin(socket.id, data.name)
+    })
+
+    socket.on('getWordTurn', function(data) {
+        //Reset
+        allParty[data][5] = ''
+        allParty[data][7][0] = []
+        allParty[data][7][1] = []
+        allParty[data][8] = 0
+        if (allParty[data][3] === -1) {
+            allParty[data][6] = true
+            RefreshAll()
+        }
+        if (allParty[data][3] === allParty[data][1].length - 1) {
+            allParty[data][3] = 0
+        } else {
+            allParty[data][3]++
+        }
+        io.in(allParty[data][0]).emit('wordTurn', allParty[data][1][allParty[data][3]][1])
+    })
+
+    function LetterTurn(index) {
+        function AddCondition() {
+            if (allParty[index][4] === allParty[index][1].length - 1) {
+                allParty[index][4] = 0
+            } else {
+                allParty[index][4]++
+            }
+        }
+        AddCondition()
+        if (allParty[index][4] === allParty[index][3]) {
+            AddCondition()
+        }
+        return allParty[index][1][allParty[index][4]][1]
+    }
+    function WordProcess(word, letter, index) {
+        let error = true
+        if (allParty[index][7][0].length === 0) {
+            for (let i = 0; i < word.length; i++) {
+                allParty[index][7][0].push('_')
+            }
+        }
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] === word[0] || word[i] === word[word.length - 1]) {
+                allParty[index][7][0].splice(i, 1, word[i])
+            } else if (word[i].toLowerCase() === letter.toLowerCase()) {
+                allParty[index][7][0].splice(i, 1, word[i])
+                error = false
+            } else if (word[i] === ' ') {
+                allParty[index][7][0].splice(i, 1, ' ')
+            } else if (word[i] === '-') {
+                allParty[index][7][0].splice(i, 1, '-')
+            } else if (word[i] === "'") {
+                allParty[index][7][0].splice(i, 1, "'")
+            }
+        }
+        if (allParty[index][7][0].some(elem => elem === '_') !== true && allParty[index][8] !== 8) {
+            io.in(allParty[index][0]).emit('partyEnd_Win')
+        }
+        if (letter !== 'NONE' && error === true) {
+            allParty[index][7][1].push(letter)
+            allParty[index][8]++
+            io.in(allParty[index][0]).emit('mistakeLetter', allParty[index][8])
+            //error count
+            if (allParty[index][8] === 8) {
+                io.in(allParty[index][0]).emit('partyEnd_Lose', allParty[index][5])
+            }
+        }
+        //To Win Or Death
+    }
+    function newLetterChoice(index, word, letter) {
+        WordProcess(word, letter, index)
+        if (allParty[index][8] !== 8) {
+            io.in(allParty[index][0]).emit('wordProcess', {
+                word: allParty[index][7][0],
+                letter: allParty[index][7][1]
+            })
+            io.in(allParty[index][0]).emit('letterTurn', LetterTurn(index))
+        }
+    }
+    socket.on('wordIs', function(data) {
+        allParty[data.index][5] = data.word;
+        newLetterChoice(data.index, data.word, data.letter)
+    })
+
+    socket.on('newLetterChoice', function(data) {
+        newLetterChoice(data.index, allParty[data.index][5], data.letter)
     })
 
     function RefreshAll() {
@@ -99,10 +185,11 @@ io.sockets.on('connection', function(socket){
                     if (i === 0) {
                         io.in(allParty[j][0]).emit('ERROR_002')
                         allParty.splice(j, 1)
+                    } else {
+                        //PROVISOIRE QUAND QUELQU4UN QUITTE LA PARTY S'ARRETE
+                        io.in(allParty[j][0]).emit('ERROR_002')
+                        allParty.splice(j, 1)
                     }
-                    //PROVISOIRE QUAND QUELQU4UN QUITTE LA PARTY S'ARRETE
-                    io.in(allParty[j][0]).emit('ERROR_002')
-                    allParty.splice(j, 1)
 
                     RefreshAll()
                     return;
